fc1cf0d4160d6c16db3f7d9d4098e0d7c0b53345
ujimoto <d.ujimoto@corp.acall.jp>
2020-08-25 23:03:24 +0900

    feat: add APIGateway proxy response

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handle() (string, error) {
-	return "ok", nil
+func handle() (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "ok",
+	}, nil
 }
 
 func main() {
