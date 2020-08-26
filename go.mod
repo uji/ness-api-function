cf383555240d59db3713c947cbbe04ed8fadd0a8
uji <ujiprog@gmail.com>
2020-08-26 08:40:41 +0000

    feat: handle graphql from apigateway

--- go.mod
+++ go.mod
@@ -4,6 +4,12 @@ go 1.15
 
 require (
 	github.com/99designs/gqlgen v0.12.2
+	github.com/agnivade/levenshtein v1.1.0 // indirect
 	github.com/aws/aws-lambda-go v1.19.1
+	github.com/awslabs/aws-lambda-go-api-proxy v0.8.0
+	github.com/gorilla/mux v1.8.0
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/mitchellh/mapstructure v1.3.3 // indirect
+	github.com/onsi/ginkgo v1.14.0 // indirect
 	github.com/vektah/gqlparser/v2 v2.0.1
 )
--- server.go
+++ server.go
@@ -1,29 +1,35 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+	"context"
 
 	"example.com/ness-api-function/graph"
 	"example.com/ness-api-function/graph/generated"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/gorilla/mux"
 )
 
-const defaultPort = "8080"
+var muxAdpt *gorillamux.GorillaMuxAdapter
+
+func init() {
+  r := mux.NewRouter()
+  schm := generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})
+	srv := handler.NewDefaultServer(schm)
+
+	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	r.Handle("/query", srv)
+
+  muxAdpt = gorillamux.New(r)
+}
+
+func handle(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+  return muxAdpt.Proxy(req)
+}
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
-
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
-
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	lambda.Start(handle)
 }
