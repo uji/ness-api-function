9d5da62c4d6be147d1054fafda0413d983ae29a4
uji <ujiprog@gmail.com>
2020-11-01 12:44:57 +0000

    fix: encode fcm credentials file

--- infra/fbs/firebase.go
+++ infra/fbs/firebase.go
@@ -2,6 +2,7 @@ package fbs
 
 import (
 	"context"
+	"encoding/base64"
 	"errors"
 	"os"
 
@@ -11,15 +12,19 @@ import (
 )
 
 var (
-	ErrInvalidCredentialsJsonBase64 = errors.New("invalid fcm credentials json base64")
+	ErrNotFoundCredentialsJsonBase64 = errors.New("not found env value FCM_CREDENTIALS_JSON_BASE64")
 )
 
 func NewAuthClient(ctx context.Context) (*auth.Client, error) {
 	crd := os.Getenv("FCM_CREDENTIALS_JSON_BASE64")
 	if crd == "" {
-		return nil, ErrInvalidCredentialsJsonBase64
+		return nil, ErrNotFoundCredentialsJsonBase64
 	}
-	opt := option.WithCredentialsJSON([]byte(crd))
+	bytes, err := base64.StdEncoding.DecodeString(crd)
+	if err != nil {
+		return nil, err
+	}
+	opt := option.WithCredentialsJSON(bytes)
 	app, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
 		return nil, err
